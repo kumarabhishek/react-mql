d555bf0dc56bd465c46c2100b7f629d7f2fc5de8
Kumar Abhishek <kumarabhishek.opensource@github.com>
2018-08-02 19:50:09 +0530

    Missed out to push.

--- example/comp_stateless.jsx
+++ example/comp_stateless.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 const CompStateless = (props) => {
-  return <div style={{background: props.bigScreen ? '#edeeed' : '#66ee66', padding: '1rem'}}>
-    <h3>Stateless Component</h3>
-    <h4>CSS Media Query Matches:<br/>{JSON.stringify(props, 4)}</h4>
-  </div>
+	return <div style={{ background: props.bigScreen ? '#edeeed' : '#66ee66', padding: '1rem' }}>
+		<h3>Stateless Component</h3>
+		<h4>CSS Media Query Matches:<br/>{JSON.stringify(props, 4)}</h4>
+	</div>
 };
 
 export default CompStateless;

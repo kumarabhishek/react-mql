720ffa21a457b3236dc8343976685519a58b9203
Kumar Abhishek <kumarabhishek.opensource@gmail.com>
2018-09-11 07:50:41 +0530

    Updated external config.

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,18 @@ module.exports = {
 		libraryTarget: 'umd'
 	},
 	externals : {
-		'react': 'react',
-		'react-dom': 'react-dom'
+		'react': {          
+			commonjs: "react",
+			commonjs2: "react",
+			amd: "React",
+			root: "React"
+        },      
+        'react-dom': {
+			commonjs: "react-dom",
+			commonjs2: "react-dom",
+			amd: "ReactDOM",
+			root: "ReactDOM"
+        }
 	},
 	target: 'web',
 	module: {
